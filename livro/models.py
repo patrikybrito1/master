6d8ba084268a68e7cc53eb78ee203293a26e557d
patriky1 <patrikybrito@gmail.com>
2023-03-12 22:15:53 -0300

    up

---
+++ livro/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from datetime import date
+from usuarios.models import Usuario
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Livros(models.Model):
+    nome = models.CharField(max_length=80)
+    autor = models.CharField(max_length=30)
+    co_autor = models.CharField(max_length=30, blank= True, null=True)
+    data_cadastro = models.DateField(default=date.today)
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length=30, blank= True, null=True)
+    data_emprestimo = models.DateField(blank= True, null=True)
+    data_devolucao = models.DateField(blank= True, null=True)
+    tempo_duracao = models.DateField(blank= True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING, blank= True, null=True)
+
+
+
+    class Meta:
+        verbose_name='Livro'
+    
+    def __str__(self) -> str:
+        return self.nome
---
+++ livro/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from usuarios.models import Usuario
+from .models import Livros
+
+
+def home (request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        livros = Livros.objects.all()
+        return render(request, 'home.html', {'livros':livros})
+    else:
+        return redirect('/auth/login/?status=2')
+    
+def ver_livros (request, id):
+    livros = Livros.objects.get(id = id)
+    return render(request, 'ver_livro.html', {'livro': livros})
