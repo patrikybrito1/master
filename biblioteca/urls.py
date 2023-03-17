baa9cfc05888aefa55e0bd3fde9edbe107819c35
patriky1 <patrikybrito@gmail.com>
2023-03-16 22:04:28 -0300

    urls update

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,11 +1,10 @@
-
+from usuarios.views import login
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
-    path ('auth/', include('usuarios.urls'))
+    path ('auth/', include('usuarios.urls')),
+    path('', login),
 ]
