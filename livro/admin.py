87d123bdd73d8d094ef55ea3072c9d67f793b0be
patriky1 <patrikybrito@gmail.com>
2023-03-18 02:59:15 -0300

    feat: add class

--- livro/admin.py
+++ livro/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Livros, Categoria
+from .models import Livros, Categoria, Turma
 
 class SearchBook(admin.ModelAdmin):
     search_fields = ['nome']
 
 admin.site.register(Categoria, SearchBook)
 admin.site.register(Livros, SearchBook)
+admin.site.register(Turma)
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,28 @@
 from django.db import models
- 
+
+class Turma(models.Model):
+  
+    nome_turma = models.CharField(max_length=50,blank=True, null=True)
+    
+  
+#    color = models.CharField(max_length=6, choices=COLOR_CHOICES, default='green')
+    def __str__(self) -> str:
+       return self.nome_turma
+    
 class Usuario(models.Model):
     nome = models.CharField(max_length=30)
     email = models.EmailField()
     senha = models.CharField(max_length=64)
 
+    nome_turma = models.ForeignKey(
+        Turma, on_delete=models.DO_NOTHING, blank=True, null=True)
+    
+    def __str__(self) -> str:
+       return self.nome_turma
+
+
     def __str__(self) -> str:
         return self.nome
+
+
+
