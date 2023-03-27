7d2b886641d2132698f2392473206ce1a1207fce
patriky1 <patrikybrito@gmail.com>
2023-03-27 20:29:56 -0300

    deploy

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#d&xj-j^$hqo5y4n2_k(0yqdc^#4yx+^06e$r(z2&(9jfhy5!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
