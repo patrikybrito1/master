cf85621e346d8796c941ce6b11da238f8e9fe7c1
patriky1 <patrikybrito@gmail.com>
2023-03-28 02:10:45 -0300

    feat: add cors

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -126,7 +126,9 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
