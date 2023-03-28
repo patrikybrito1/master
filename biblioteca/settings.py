f224b31e6b233dfe1199133db8ffc95636420729
patriky1 <patrikybrito@gmail.com>
2023-03-28 02:17:19 -0300

    feat: add cors

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#d&xj-j^$hqo5y4n2_k(0yqdc^#4yx+^06e$r(z2&(9jfhy5!@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_FINDER = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
