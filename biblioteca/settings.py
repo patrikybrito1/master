7318918ff14b093787cca78312325f2e67108768
patriky1 <patrikybrito@gmail.com>
2023-03-27 23:30:25 -0300

    feat: add cors

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#d&xj-j^$hqo5y4n2_k(0yqdc^#4yx+^06e$r(z2&(9jfhy5!@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
