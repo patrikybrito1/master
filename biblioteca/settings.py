9514a8cdace89dafba64ccaabc4a0b1a2a47e786
patriky1 <patrikybrito@gmail.com>
2023-03-27 23:04:06 -0300

    feat: add cors

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -130,4 +130,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://biblioteca-production-fbf2.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://biblioteca-production-fbf2.up.railway.app", "https://biblioteca-production-fbf2.up.railway.app/admin"]
